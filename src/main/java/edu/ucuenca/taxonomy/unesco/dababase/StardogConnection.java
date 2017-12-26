aa462e876649b608a4f90f8f2fbaa2b895337cd2
Xavier Sumba <c.uent@hotmail.com>
2017-12-26 12:47:31 -0500

    stardog connection

--- src/main/java/edu/ucuenca/taxonomy/unesco/dababase/StardogConnection.java
+++ src/main/java/edu/ucuenca/taxonomy/unesco/dababase/StardogConnection.java
@@ -16,45 +16,50 @@
  */
 package edu.ucuenca.taxonomy.unesco.dababase;
 
-//import com.blazegraph.gremlin.embedded.BlazeGraphEmbedded;
-//import com.blazegraph.gremlin.embedded.BlazeGraphFactory;
 import com.complexible.stardog.gremlin.StardogGraphConfiguration;
 import com.complexible.stardog.gremlin.StardogGraphConfiguration.Builder;
 import com.complexible.stardog.gremlin.StardogGraphFactory;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Graph;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.openrdf.repository.RepositoryException;
 
 /**
  *
  * @author Xavier Sumba <xavier.sumba93@ucuenca.ec>
  */
-public class StardogConnection {
+public class StardogConnection implements AutoCloseable {
 
-    public static void main(String[] args) throws RepositoryException, Exception {
-//        final BlazeGraphEmbedded graph = BlazeGraphFactory.open("coco.jnl");
-//        Graph coco1 = IOGraph.read("coco2.graphml");
-//        graph.bulkLoad(coco1);
-//        graph.commit();
-//        GraphTraversalSource g = graph.traversal();
+    private static StardogConnection stardog;
+    private final static String DATABASE = "http://localhost:5820/myDB";
+    private final static String USER = "admin";
+    private final static String PASSWD = "admin";
+    private final static String BASE_IRI = "http://redi.cedi.edu.ec/resource";
+    private final static String CONTEXT = "http://redi.cedi.edu.ec/context/redi";
+
+    private final Graph graph;
+
+    private StardogConnection() {
         Builder conf = StardogGraphConfiguration.builder();
-        conf.connectionString("http://localhost:5820/myDB")
-                .credentials("admin", "admin").baseIRI("http://redi.cedi.edu.ec/");
-
-        try (Graph graph = StardogGraphFactory.open(conf.build())) {
-            Vertex marko = graph.addVertex("label", "person", "id", 1, "name", "marko", "age", 29);
-            System.out.println(marko.values());
-            GraphTraversalSource g = graph.traversal();
-            System.out.println(g.V().has("id", 1).hasNext());
-//            Preprocessing p = Preprocessing.getInstance();
-//            p.entitiesEnrichment("http://dbpedia.org/resource/Computer_science", graph);
-        }
+        conf.connectionString(DATABASE)
+                .credentials(USER, PASSWD)
+                .baseIRI(BASE_IRI)
+                .namedGraph(CONTEXT);
+        graph = StardogGraphFactory.open(conf.build());
     }
 
-//    public static BlazeGraphEmbedded open(final File file) throws Exception {
-//        final String journal = file.getAbsolutePath();
-//        final BlazeGraphEmbedded graph = BlazeGraphFactory.open(journal);
-//        return graph;
-//    }
+    public static StardogConnection intance() {
+        if (stardog == null) {
+            return new StardogConnection();
+        }
+        return stardog;
+    }
+
+    public Graph graph() {
+        return graph;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (graph != null) {
+            graph.close();
+        }
+    }
 }
